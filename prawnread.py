bae83648c40f31d7e7b61cba13854ee988a11cbc
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-10-06 16:13:57 +1000

    add os
    add os

--- prawnread.py
+++ prawnread.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import os
+file_path =  "inputdata.txt"  # Streamlit 서버가 실행되는 디렉토리에 생성됩니다.
+
 with open("inputdata.txt", 'r') as file:
        content = file.read()
        st.text(content)
