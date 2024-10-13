ed550aadbbcea87d43f3daf76ea0c52c61ab48fc
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-10-13 20:56:38 +1000

    Modify screen
    Modify screen

--- sqlightread.py
+++ sqlightread.py
@@ -1,4 +1,6 @@
+import streamlit as st
 import sqlite3
+import pandas as pd
 
 # 데이터베이스 연결 (없으면 생성됨)
 conn = sqlite3.connect('kevin.db')
@@ -7,11 +9,19 @@ conn = sqlite3.connect('kevin.db')
 c = conn.cursor()
 
 #  **데이터 읽기:**
-c.execute('SELECT * FROM buyer')
-rows = c.fetchall()
+#c.execute('SELECT * FROM buyer')
+#rows = c.fetchall()
+#for row in rows:
+#    print(row)
 
-for row in rows:
-    print(row)
+
+# SQL 쿼리 실행
+query = "SELECT * FROM buyer"
+df = pd.read_sql_query(query, conn)
+
+# 데이터프레임을 Streamlit으로 표시
+st.title('Customer Request Data')
+st.write(df)
 
 # 연결 종료
 conn.close()
